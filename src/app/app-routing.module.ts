b7b52606832cbc12d2f1694bf819d32d630be428
dougpiv <dapivoto@gmail.com>
2022-05-08 20:53:18 -0300

    aula 19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 
 const routes: Routes = [
   { path: '', component: FirstComponentComponent },
   { path: 'list', component: ListRenderComponent },
   { path: 'if', component: IfRenderComponent },
+  { path: 'list/:id', component: ItemDetailComponent },
 ];
 
 @NgModule({
